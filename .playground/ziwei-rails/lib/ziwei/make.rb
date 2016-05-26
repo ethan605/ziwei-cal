486ccfcedb373cd52fe67dd9fc9f6d658fd82212
Ethan N <thanhnx.605@gmail.com>
2016-05-26 16:12:20 +0700

    Refactor

---
+++ .playground/ziwei-rails/lib/ziwei/make.rb
@@ -0,0 +1,15 @@
+from_dir = "#{Dir.pwd}"
+to_dir = "#{Dir.pwd}/../../../.."
+dir_name = "ziwei-ruby"
+`cp -r #{from_dir} #{to_dir}/#{dir_name}`
+
+constant_file_content = %{
+module Ziwei
+  module Constants
+    ROOT_DIR = "."
+  end
+end
+}
+
+File.write("#{to_dir}/#{dir_name}/constants.rb", constant_file_content)
+`rm #{to_dir}/#{dir_name}/make.rb`
