2cac0020b50f7a6bcfa5b1131b6660f5e85583dc
Ethan N <thanhnx.605@gmail.com>
2016-05-27 19:20:16 +0700

    [js] Add Six deadly & lucky stars calculations

---
+++ .playground/ziwei-js/utils/calc_six_deadly_stars.js
@@ -0,0 +1,45 @@
+_Ziwei_Calculator.prototype.calcKhongKiepPositions = function(birthHour) {
+  var hoiIndex = Ziwei.Configs.Branches.Indexes['hoi'];
+  var birthHourIndex = Ziwei.Configs.Branches.Indexes[birthHour];
+
+  var diaKhongIndex = hoiIndex.limitInc(-birthHourIndex+1);
+  var diaKiepIndex = hoiIndex.limitInc(birthHourIndex-1);
+
+  return [
+    Ziwei.Configs.Branches.Orders[diaKhongIndex],
+    Ziwei.Configs.Branches.Orders[diaKiepIndex]
+  ]
+};
+
+_Ziwei_Calculator.prototype.calcKinhDaPositions = function(locTonPosition) {
+  var locTonIndex = Ziwei.Configs.Branches.Indexes[locTonPosition];
+
+  var kinhDuongIndex = locTonIndex.limitInc();
+  var daLaIndex = locTonIndex.limitInc(-1);
+
+  return [
+    Ziwei.Configs.Branches.Orders[kinhDuongIndex],
+    Ziwei.Configs.Branches.Orders[daLaIndex]
+  ]
+};
+
+_Ziwei_Calculator.prototype.calcSixDeadlyStars = function(argument) {
+  var [diaKhongPosition, diaKiepPosition] = this.calcKhongKiepPositions(birthHour);
+  var [kinhDuongPosition, daLaPosition] = this.calcKinhDaPositions(locTonPosition);
+
+  var starsPositions = {};
+
+  [
+    [diaKhongPosition, 'dia_khong'],
+    [diaKiepPosition, 'dia_kiep'],
+    [kinhDuongPosition, 'kinh_duong'],
+    [daLaPosition, 'da_la']
+  ].forEach((position, star) => {
+    if (starsPositions[position] === undefined)
+      starsPositions[position] = [];
+
+    starsPositions[position].push(star);
+  });
+
+  return starsPositions;
+};
